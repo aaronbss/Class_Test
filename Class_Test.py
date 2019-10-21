dbb3dc4df081bf77a598a9f61b0f0decb3c2f151
aaronbss <55691291+aaronbss@users.noreply.github.com>
2019-10-21 10:03:44 +0100

    Create Class_Test.py

---
+++ Class_Test.py
@@ -0,0 +1,18 @@
+class Student:
+    def __init__(self, stud_num, stud_nam, course_num):
+        self.stud_num = [8]
+        self.stud_nam = []
+        self.course_num = [7]
+
+        for i in range(1, 4):
+            self.stud_num[i] = input("Please enter the Student Number ")
+            self.stud_nam[i] = input("Please enter the Student Name")
+            self.course_num = input()
+
+
+
+
+
+
+
+obj = Student("10758","Aaron", "20A")
