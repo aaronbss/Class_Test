10a1a31f612e027d16358d01734e55ff28df89e9
aaronbss <55691291+aaronbss@users.noreply.github.com>
2019-10-21 10:13:29 +0100

    Update Class_Test.py

--- Class_Test.py
+++ Class_Test.py
@@ -3,11 +3,15 @@ class Student:
         self.stud_num = [8]
         self.stud_nam = []
         self.course_num = [7]
-
-        for i in range(1, 4):
+        n = int(input("Please enter the number of students ou want to register"))
+        for i in range(1, n+1):
             self.stud_num[i] = input("Please enter the Student Number ")
             self.stud_nam[i] = input("Please enter the Student Name")
-            self.course_num = input()
+            self.course_num = input("Please enter the Course Number")
+
+    def retrieve(self):
+        
+
 
 
 
