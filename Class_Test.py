1dcd7e6911e0771f6e867a37dc5d2d3e33a80b4e
aaronbss <55691291+aaronbss@users.noreply.github.com>
2019-11-02 13:36:05 +0000

    Update Class_Test.py

--- Class_Test.py
+++ Class_Test.py
@@ -1,24 +1,27 @@
 class Student:
-    def __init__(self):
-        self.stud_num = []
-        self.stud_nam = []
-        self.course_num = []
-        n = int(input("Please enter the number of students ou want to register"))
+
+    def __init__(self, a=[], b=[], c=[]):
+        self.stud_num = a
+        self.stud_nam = b
+        self.course_num = c
+
+    def add_value(self):
+        n = int(input("Please enter the number of students you want to register"))
         for i in range(1, n+1):
-            self.stud_num[i] = input("Please enter the Student Number ")
-            self.stud_nam[i] = input("Please enter the Student Name")
-            self.course_num = input("Please enter the Course Number")
+            self.stud_num[i].append(input("Please enter the Student number"))
+            self.stud_nam[i].append(input("Please enter student name"))
+            self.course_num[i].append(input("Please enter the course ID"))
 
     def retrieve(self):
-        for i in range(2, len(self.stud_num)):
-            if self.stud_num[1]<self.stud_num[i+1]:
+        for k in range(2, len(self.stud_num)):
+            if self.stud_num[1] < self.stud_num[k+1]:
                 continue
             else:
-                self.stud_num[1] = self.stud_num[i]
+                self.stud_num[1].replace(self.stud_num[k])
+                self.stud_nam[1].replace(self.stud_nam[k])
+                self.course_num[1].replace(self.course_num[k])
 
-        print("The lowest student number retrieved is", self.stud_num[1])
+        print("The lowest student number, Name, Curse ID retrieved is", self.stud_num[1], self.stud_nam[1], self.course_num[1])
 
 
-num = retrieve()
-
-###x
+obj = Student()
