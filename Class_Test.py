c288e2cecbfa4da9db0461162ec3842c3579370e
aaronbss <55691291+aaronbss@users.noreply.github.com>
2019-11-02 14:53:36 +0000

    Update Class_Test.py

--- Class_Test.py
+++ Class_Test.py
@@ -1,6 +1,3 @@
-import numpy as np
-
-
 class Student:
 
     def __init__(self, a=[], b=[], c=[]):
@@ -20,16 +17,16 @@ class Student:
         print(self.course_num)
 
     def retrieve(self):
-        for j in range(1, 2):
-            for k in range(2, len(self.stud_num)):
+        for j in range(0, 1):
+            for k in range(1, len(self.stud_num)):
                 if self.stud_num[j] < self.stud_num[k]:
                     continue
                 else:
-                    self.stud_num[j].replace(self.stud_num[k])
-                    self.stud_nam[j].replace(self.stud_nam[k])
-                    self.course_num[j].replace(self.course_num[k])
+                    self.stud_num[j] = (self.stud_num[k])
+                    self.stud_nam[j] = (self.stud_nam[k])
+                    self.course_num[j] = (self.course_num[k])
 
-        for i in range(1, 2):
+        for i in range(0, 1):
             print("The lowest student number, Name, Course ID retrieved is", self.stud_num[i], self.stud_nam[i], self.course_num[i])
 
 
