480a6e50f23f97f7d03e24525f48c6e582d6119b
aaronbss <55691291+aaronbss@users.noreply.github.com>
2019-11-02 17:28:34 +0000

    Create Class_Test_Selection_Sort.py

---
+++ Class_Test_Selection_Sort.py
@@ -0,0 +1,36 @@
+class Student:
+
+    def __init__(self, a=[], b=[], c=[]):
+        self.stud_num = a
+        self.stud_nam = b
+        self.course_num = c
+
+    def add_value(self):
+        n = int(input("Please enter the number of students you want to register"))
+        for i in range(1, n+1):
+            self.stud_num.append(int(input("Please enter the student number")))
+            self.stud_nam.append(input("Please enter student name"))
+            self.course_num.append(int(input("Please enter the course ID")))
+
+        print(self.stud_nam)
+        print(self.stud_num)
+        print(self.course_num)
+
+    def retrieve(self):
+        for i in range(0, len(self.stud_num) - 1):
+            min_index = i
+            for j in range(i + 1, len(self.stud_num)):
+                if self.stud_num[j] < self.stud_num[min_index]:
+                    min_index = j
+            if min_index != i:
+                self.stud_num[i], self.stud_num[min_index] = self.stud_num[min_index], self.stud_num[i]
+                self.stud_nam[i], self.stud_nam[min_index] = self.stud_nam[min_index], self.stud_nam[i]
+                self.course_num[i], self.course_num[min_index] = self.course_num[min_index], self.course_num[i]
+
+        for i in range(0, 1):
+            print("The lowest student number, Name, Course ID retrieved is", self.stud_num[i], self.stud_nam[i], self.course_num[i])
+
+
+obj = Student()
+obj.add_value()
+obj.retrieve()
