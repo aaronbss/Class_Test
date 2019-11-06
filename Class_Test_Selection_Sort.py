bcd223cd41898ee0f768cf3bed3dbe932aa3c855
aaronbss <55691291+aaronbss@users.noreply.github.com>
2019-11-06 23:05:48 +0000

    Update Class_Test_Selection_Sort.py

--- Class_Test_Selection_Sort.py
+++ Class_Test_Selection_Sort.py
@@ -4,6 +4,8 @@ order to sort, fetch & delete entries. The sorting method used here is selection
 time and also sorts in an ascending order making it easier for the program to eliminate the student details with the
 lowest student number"""
 
+import time
+
 
 class Student:
 
@@ -15,19 +17,17 @@ class Student:
     def add_value(self):
         n = int(input("Please enter the number of students you want to register"))
         for i in range(1, n+1):
-            try:                                                            # Implemented Try & Catch in order to
-                a = int(input("Please enter the student number"))           # let the user know when a wrong value has
-                if len(str(a)) < 9:                                         # been entered.
-                    self.stud_num.append(a)
-            except:
+            a = int(input("Please enter the student number"))
+            if len(str(a)) < 9:
+                self.stud_num.append(a)
+            else:
                 print("You have entered wrong student number")
                 break
             self.stud_nam.append(input("Please enter student name"))
-            try:
-                b = int(input("Please enter the course ID"))
-                if len(str(b)) < 8:
-                    self.course_num.append(b)
-            except:
+            b = int(input("Please enter the course ID"))
+            if len(str(b)) < 8:
+                self.course_num.append(b)
+            else:
                 print("You have entered a wrong course id")
                 break
 
@@ -36,6 +36,7 @@ class Student:
         print("Course Number", self.course_num)
 
     def retrieve(self):
+        start_time = time.perf_counter()
         for i in range(0, len(self.stud_num) - 1):
             min_index = i
             for j in range(i + 1, len(self.stud_num)):
@@ -56,6 +57,7 @@ class Student:
         print("Names of students", self.stud_nam)
         print("Student Number", self.stud_num)
         print("Course Number", self.course_num)
+        print("--- %s seconds ---" % (round(time.perf_counter()) - start_time))           # Retrieval Time calculation
 
 
 obj = Student()
