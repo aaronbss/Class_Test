52ed4136f13c6fef76dc338b2af6757d13812fe3
aaronbss <55691291+aaronbss@users.noreply.github.com>
2019-11-06 22:29:36 +0000

    Update Class_Test_Selection_Sort.py

--- Class_Test_Selection_Sort.py
+++ Class_Test_Selection_Sort.py
@@ -1,3 +1,10 @@
+# https://github.com/aaronbss/Class_Test
+""" In this program i am taking the input of the student details in 3 different arrays and then manipulating it in
+order to sort, fetch & delete entries. The sorting method used here is selection sort since it has a quick retrieve
+time and also sorts in an ascending order making it easier for the program to eliminate the student details with the
+lowest student number"""
+
+
 class Student:
 
     def __init__(self, a=[], b=[], c=[]):
@@ -8,7 +15,12 @@ class Student:
     def add_value(self):
         n = int(input("Please enter the number of students you want to register"))
         for i in range(1, n+1):
-            self.stud_num.append(int(input("Please enter the student number")))
+            try:
+                a = int(input("Please enter the student number"))
+                if len(a) < 9:
+                    self.stud_num.append(a)
+            except:
+                print("You have entered wrong student number")
             self.stud_nam.append(input("Please enter student name"))
             self.course_num.append(int(input("Please enter the course ID")))
 
@@ -20,7 +32,7 @@ class Student:
         for i in range(0, len(self.stud_num) - 1):
             min_index = i
             for j in range(i + 1, len(self.stud_num)):
-                if self.stud_num[j] < self.stud_num[min_index]:                                    
+                if self.stud_num[j] < self.stud_num[min_index]:
                     min_index = j
             if min_index != i:
                 self.stud_num[i], self.stud_num[min_index] = self.stud_num[min_index], self.stud_num[i]
@@ -38,8 +50,6 @@ class Student:
         print("Student Number", self.stud_num)
         print("Course Number", self.course_num)
 
-
-# https://github.com/aaronbss/Class_Test
 
 obj = Student()
 obj.add_value()
