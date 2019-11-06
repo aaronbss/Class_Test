4b16a7d0822902f7718f593d9e9d5c9dbb48505c
aaronbss <55691291+aaronbss@users.noreply.github.com>
2019-11-06 23:21:18 +0000

    Update Class_Test_Selection_Sort.py

--- Class_Test_Selection_Sort.py
+++ Class_Test_Selection_Sort.py
@@ -4,12 +4,12 @@ order to sort, fetch & delete entries. The sorting method used here is selection
 time and also sorts in an ascending order making it easier for the program to eliminate the student details with the
 lowest student number"""
 
-import time
+import time     # Importing the time function to display the sorting time.
 
 
 class Student:
 
-    def __init__(self, a=[], b=[], c=[]):
+    def __init__(self, a=[], b=[], c=[]):            # Initialising the init function by specifying the input variables
         self.stud_num = a
         self.stud_nam = b
         self.course_num = c
@@ -18,11 +18,11 @@ class Student:
         n = int(input("Please enter the number of students you want to register"))
         for i in range(1, n+1):
             a = int(input("Please enter the student number"))
-            if len(str(a)) < 9:
-                self.stud_num.append(a)
+            if len(str(a)) < 9:                               # Ensuring the length of the entered number is less than 8
+                self.stud_num.append(a)                       # Adding the number to the list
             else:
-                print("You have entered wrong student number")
-                break
+                print("You have entered a wrong student number")  # Exception message
+                break                                                 # Breaks out of the loop and displays the message
             self.stud_nam.append(input("Please enter student name"))
             b = int(input("Please enter the course ID"))
             if len(str(b)) < 8:
@@ -36,13 +36,13 @@ class Student:
         print("Course Number", self.course_num)
 
     def retrieve(self):
-        start_time = time.perf_counter()
-        for i in range(0, len(self.stud_num) - 1):
-            min_index = i
-            for j in range(i + 1, len(self.stud_num)):
-                if self.stud_num[j] < self.stud_num[min_index]:
-                    min_index = j
-            if min_index != i:
+        start_time = time.perf_counter()                # Timer Started
+        for i in range(0, len(self.stud_num) - 1):      # For-loop to run the outer loop
+            min_index = i                               # Assuming the first index value to be the minimum
+            for j in range(i + 1, len(self.stud_num)):  # Second For-loop to run the inner loop
+                if self.stud_num[j] < self.stud_num[min_index]:  # Comparing value of index one with index 0
+                    min_index = j                        # if Value of index one is lower we reassign the lowest index
+            if min_index != i:                           # We interchange values so that the arrays are Synchronous
                 self.stud_num[i], self.stud_num[min_index] = self.stud_num[min_index], self.stud_num[i]
                 self.stud_nam[i], self.stud_nam[min_index] = self.stud_nam[min_index], self.stud_nam[i]
                 self.course_num[i], self.course_num[min_index] = self.course_num[min_index], self.course_num[i]
